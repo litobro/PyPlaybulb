c0fea57ef3e9468c4ef5d15823042d30a8af2154
Litobro <thomas.kyle.dang@gmail.com>
2015-11-12 01:07:33 +0000

    Started prep for PyPi distributing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='PyPlaybulb',
+      version='0.1',
+      license='MIT License',
+      url='https://github.com/litobro/PyPlaybulb',
+      download_url='https://github.com/litobro/PyPlaybulb/tarball/0.1',
+      author='Thomas Dang',
+      author_email='thomas.kyle.dang@gmail.com',
+      description='Simple (and dangerous) wrapper for MiPow Playbulb devices',
+      keywords = ['MiPow', 'Playbulb', 'Bluetooth']
+)
